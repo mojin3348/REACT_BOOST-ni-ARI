922213a96dc3d08536f1abb03f9fabaecbc9e215
mojin3348 <mojin3348@gmail.com>
2025-09-06 23:32:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const axios = require("axios");
 
 const app = express();
 app.use(express.json());
